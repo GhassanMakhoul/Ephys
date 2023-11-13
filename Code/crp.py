89abbba749f9c48667f73e9bbbc0718c660615b6
GhassanMakhoul <ghassan.makhoul@vanderbilt.edu>
2023-11-13 10:41:31 -0600

    continuing crossproj exploration worried that there is no peak

--- Code/crp.py
+++ Code/crp.py
@@ -1,3 +1,21 @@
 import mne
 import numpy as np
 import pandas as pd
+
+def construct_spes_df(spes_trains, contact_labels, fs):
+    assert len(contact_labels) == min(spes_trains.shape)
+    if np.argmax(spes_trains.shape) != 0:
+        spes_trains = spes_trains.T
+    nsamps,ch = spes_trains.shape
+    t = np.arange(nsamps)/fs
+    df = pd.DataFrame(columns=contact_labels, index =t, data=spes_trains)
+    return  df
+
+def plot_channels(spes_df , channel_list):
+    nrows = len(channel_list)
+    fig, axes = plt.subplots(nrows=nrows, ncols=1,sharex=True)
+    for i,ch in enumerate(channel_list):
+        ax = axes[i]
+        sns.lineplot(x=spes_df.index, y=spes_df[ch], ax=ax)
+    
+    
