073014f649ed0cf15ec89bcd382d3f90ed79ce84
Price Withers <christopher.p.withers@vanderbilt.edu>
2024-08-07 16:58:09 -0500

    Collects stats on FPAC analysis

--- Code/dynamic_ISH/code/run_verbose_flow.py
+++ Code/dynamic_ISH/code/run_verbose_flow.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import logging
-
-logging.getLogger('mat73').setLevel(logging.CRITICAL)
 import os
 logging.getLogger('mat73').setLevel(logging.CRITICAL)
 
@@ -22,10 +20,12 @@ if __name__ == '__main__':
 
     # load multiple patients
     agg_df_lst = []
+    stats_dict = {}
+    stats_keys = ['patID','eventID','FPAC_bips','other_bips']
     files = glob.glob(os.path.join(DATA_DIR,"*flow*pat*.csv"))
     files = [f for f in files if ('verbose' in f)]
 
-    for f in files:
+    for f in files[0:2]:
         flow_df = pd.read_csv(os.path.join(DATA_DIR,f))
         flow_df = flow_df[flow_df.freq_band == 'alpha']
         flow_df = flow_df[flow_df.source == 'nz']
@@ -42,6 +42,16 @@ if __name__ == '__main__':
         flow_df['FPAC'] = flow_df.parallel_apply(lambda x: (x.region in lut_data), axis=1)
         flow_df = center_onset(flow_df, **{'mid_sz_length':20})
         flow_df.dropna()
+
+        # collect stats
+        for event in flow_df.eventID.unique():
+            stat_df = flow_df.copy()
+            stat_df = stat_df[(flow_df.period == 0) & (flow_df.eventID == event) & (flow_df.target == 'nz')]
+            n_fpac = stat_df[stat_df.FPAC].shape[0]
+            n_other = stat_df[~stat_df.FPAC].shape[0]
+            for key, val in zip(stats_keys,(patID,event,n_fpac,n_other)):
+                stats_dict.setdefault(key,[]).append(val)
+        del stat_df
 
         agg_df = agg_verbose_df(
             flow_df,
@@ -61,3 +71,6 @@ if __name__ == '__main__':
     grp_flow_df = pd.concat(agg_df_lst)
     grp_flow_df['src_trgt'] = grp_flow_df.parallel_apply(lambda x : "nz_"+x['target'], axis=1)
     grp_flow_df.to_csv(f'{DATA_DIR}/verbose_grp_flow.csv', index=False)
+
+    stats_df = pd.DataFrame.from_dict(stats_dict)
+    stats_df.to_csv(f'{DATA_DIR}/stats_verbose_grp_flow.csv', index=False)
