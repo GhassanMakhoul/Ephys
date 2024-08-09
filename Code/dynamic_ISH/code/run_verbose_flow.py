aee64ee45eca431eda9915ddc51ddfea0a762117
Price Withers <christopher.p.withers@vanderbilt.edu>
2024-08-09 11:56:16 -0500

    Improve functionality and logging of run_verbose_flow

--- Code/dynamic_ISH/code/run_verbose_flow.py
+++ Code/dynamic_ISH/code/run_verbose_flow.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import argparse
+from loguru import logger
 import logging
 import os
+import sys
 logging.getLogger('mat73').setLevel(logging.CRITICAL)
 
 import pandas as pd
@@ -11,6 +14,28 @@ from connectivity_dynamics import *
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='create summary of flow from/to a specified source/target'
+                                                 '. user can only specify source or target. defaults to '
+                                                 'target = soz')
+    parser.add_argument("-s", "--source", choices=['soz','pz','nz'], default='', help="source of flow")
+    parser.add_argument("-t", "--target", choices=['soz','pz','nz'], default='', help="target of flow")
+    args = parser.parse_args()
+
+    if (len(args.source) > 0) & (len(args.target) > 0):
+        # user cannot specify both source and target
+        logger.error('Must specify either source or target, not both')
+        sys.exit()
+    elif (len(args.source) == 0) & (len(args.target) == 0):
+        # if user doesn't specify source or target, defaults to target=soz
+        args.target = 'soz'
+
+    if args.source:
+        retain_col = 'target'
+        logger.info(f'Source set to {args.source}')
+    else:
+        retain_col = 'source'
+        logger.info(f'Target set to {args.target}')
 
     ## get FPAC regions and store in lut_data
     fpac_lut = pd.read_csv('FPAC_lut.txt')
@@ -28,10 +53,14 @@ if __name__ == '__main__':
     for f in files[0:2]:
         flow_df = pd.read_csv(os.path.join(DATA_DIR,f))
         flow_df = flow_df[flow_df.freq_band == 'alpha']
-        flow_df = flow_df[flow_df.source == 'nz']
+        if args.source:
+            flow_df = flow_df[flow_df.source == args.source]
+        else:
+            flow_df = flow_df[flow_df.target == args.target]
 
         # get patient's bip to region dictionary
         patID = flow_df.patID.iloc[0]
+        logger.info(f'Running patient {patID}')
         mat_paths = glob.glob(f'/mnt/ernie_main/Ghassan/ephys/data/connectivity/{patID}/{patID}*PDC.mat')
         obj = load_mat(mat_paths[0])
         regions = get_atlas_regions(obj)
@@ -45,8 +74,14 @@ if __name__ == '__main__':
 
         # collect stats
         for event in flow_df.eventID.unique():
+            logger.info(f'+Working on event {event}+')
             stat_df = flow_df.copy()
-            stat_df = stat_df[(flow_df.period == 0) & (flow_df.eventID == event) & (flow_df.target == 'nz')]
+            stat_df = stat_df[(stat_df.period == 0) & (stat_df.eventID == event)]
+            if args.source:
+                stat_df = stat_df[stat_df.target == 'nz']
+            else:
+                stat_df = stat_df[stat_df.source == 'nz']
+
             n_fpac = stat_df[stat_df.FPAC].shape[0]
             n_other = stat_df[~stat_df.FPAC].shape[0]
             for key, val in zip(stats_keys,(patID,event,n_fpac,n_other)):
@@ -56,21 +91,31 @@ if __name__ == '__main__':
         agg_df = agg_verbose_df(
             flow_df,
             measure_cols = ['value'],
-            categorical_cols = ['target','win_sz_st_end','eventID','patID','sz_type','FPAC']
+            categorical_cols = [retain_col,'win_sz_st_end','eventID','patID','sz_type','FPAC']
         )
 
         agg_df = agg_verbose_df(
             agg_df,
             measure_cols = ['value'],
-            categorical_cols = ['patID','sz_type','target','win_sz_st_end','FPAC']
+            categorical_cols = ['patID','sz_type',retain_col,'win_sz_st_end','FPAC']
         )
 
         del flow_df
         agg_df_lst.append(agg_df)
 
     grp_flow_df = pd.concat(agg_df_lst)
-    grp_flow_df['src_trgt'] = grp_flow_df.parallel_apply(lambda x : "nz_"+x['target'], axis=1)
-    grp_flow_df.to_csv(f'{DATA_DIR}/verbose_grp_flow.csv', index=False)
+
+    if args.source:
+        grp_flow_df['src_trgt'] = grp_flow_df.parallel_apply(lambda x : f"{args.source}_"+x['target'], axis=1)
+        file_suffix = f'source_{args.source}'
+    else:
+        grp_flow_df['src_trgt'] = grp_flow_df.parallel_apply(lambda x : x['source']+f"_{args.target}", axis=1)
+        file_suffix = f'target_{args.target}'
+
+    grp_flow_df.to_csv(f'{DATA_DIR}/verbose_grp_flow_{file_suffix}.csv', index=False)
+
+    logger.success(f'Created summary file at {DATA_DIR}/verbose_grp_flow_{file_suffix}.csv')
 
     stats_df = pd.DataFrame.from_dict(stats_dict)
-    stats_df.to_csv(f'{DATA_DIR}/stats_verbose_grp_flow.csv', index=False)
+    stats_df.to_csv(f'{DATA_DIR}/stats_verbose_grp_flow_{file_suffix}.csv', index=False)
+    logger.success(f'Created stats file at {DATA_DIR}/stats_verbose_grp_flow_{file_suffix}.csv')
